64aa7cc6707d0f40151ea5e8d69fbd7b22bf70d4
leekwe <leekwe@163.com>
2023-10-07 15:26:25 +0800

    feat: 通用返回值

--- src/weixin/weixin.controller.ts
+++ src/weixin/weixin.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { WeixinService } from './weixin.service';
 import { JscodeToSessionDto } from './dto/jscode-to-session';
 import { SkipAuth } from 'src/user/metadata';
+import { Ret } from 'src/common/ret';
 
 @Controller('weixin')
 export class WeixinController {
@@ -9,7 +10,8 @@ export class WeixinController {
 
   @Post('jscode2session')
   @SkipAuth()
-  create(@Body() jsonToSessionDto: JscodeToSessionDto) {
-    return this.weixinService.jscode2session(jsonToSessionDto);
+  async create(@Body() jsonToSessionDto: JscodeToSessionDto) {
+    const res = await this.weixinService.jscode2session(jsonToSessionDto);
+    return Ret.ok(res)
   }
 }
